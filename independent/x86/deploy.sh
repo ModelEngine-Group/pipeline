09b87073599766079e9bbb4c07b035f00cde8bcf
desAweis <desaweis007@gmail.com>
2025-09-18 22:36:39 +0800

    [docker-hub] push images to aliyun hub

--- independent/x86/deploy.sh
+++ independent/x86/deploy.sh
@@ -2,6 +2,7 @@
 set -eux
 export WORKSPACE=$(cd "$(dirname "$(readlink -f "$0")")" && pwd)
 export VERSION=${1:-"opensource-default"}
+export REPO="crpi-62znuv6vkgxcv731.cn-hangzhou.personal.cr.aliyuncs.com/modelengine-hub"
 
 echo "=== Deploying... ==="
 cd ${WORKSPACE}/package
@@ -10,11 +11,11 @@ mkdir -p appengine/fit-runtime
 mkdir -p appengine/jade-db
 mkdir -p appengine/log
 
-docker tag app-builder:${VERSION} modelengine/app-builder:${VERSION}
-docker tag postgres:15.2-${VERSION} modelengine/postgres:15.2-${VERSION}
-docker tag jade-web:${VERSION} modelengine/jade-web:${VERSION}
-docker tag fit-runtime-python:${VERSION} modelengine/fit-runtime-python:${VERSION}
-docker tag fit-runtime-java:${VERSION} modelengine/fit-runtime-java:${VERSION}
+docker tag app-builder:${VERSION} ${REPO}/app-builder:${VERSION}
+docker tag postgres:15.2-${VERSION} ${REPO}/postgres:15.2-${VERSION}
+docker tag jade-web:${VERSION} ${REPO}/jade-web:${VERSION}
+docker tag fit-runtime-python:${VERSION} ${REPO}/fit-runtime-python:${VERSION}
+docker tag fit-runtime-java:${VERSION} ${REPO}/fit-runtime-java:${VERSION}
 
 echo "Starting service..."
 docker-compose up -d
--- independent/x86/push_images.sh
+++ independent/x86/push_images.sh
@@ -3,17 +3,21 @@
 set -eux
 
 export VERSION=${1:-"opensource-default"}
+export REPO="crpi-62znuv6vkgxcv731.cn-hangzhou.personal.cr.aliyuncs.com/modelengine-hub"
 
-docker login
+docker login --username=nick2701484369 crpi-62znuv6vkgxcv731.cn-hangzhou.personal.cr.aliyuncs.com
 
-docker tag app-builder:${VERSION} modelengine/app-builder:${VERSION}
-docker tag postgres:15.2-${VERSION} modelengine/postgres:15.2-${VERSION}
-docker tag jade-web:${VERSION} modelengine/jade-web:${VERSION}
-docker tag fit-runtime-python:${VERSION} modelengine/fit-runtime-python:${VERSION}
-docker tag fit-runtime-java:${VERSION} modelengine/fit-runtime-java:${VERSION}
+docker tag app-builder:${VERSION} ${REPO}/app-builder:${VERSION}
+docker push ${REPO}/app-builder:${VERSION}
 
-docker push modelengine/app-builder:${VERSION}
-docker push modelengine/postgres:15.2-${VERSION}
-docker push modelengine/jade-web:${VERSION}
-docker push modelengine/fit-runtime-python:${VERSION}
-docker push modelengine/fit-runtime-java:${VERSION}
+docker tag postgres:15.2-${VERSION} ${REPO}/postgres:15.2-${VERSION}
+docker push ${REPO}/postgres:15.2-${VERSION}
+
+docker tag jade-web:${VERSION} ${REPO}/jade-web:${VERSION}
+docker push ${REPO}/jade-web:${VERSION}
+
+docker tag fit-runtime-python:${VERSION} ${REPO}/fit-runtime-python:${VERSION}
+docker push ${REPO}/fit-runtime-python:${VERSION}
+
+docker tag fit-runtime-java:${VERSION} ${REPO}/fit-runtime-java:${VERSION}
+docker push ${REPO}/fit-runtime-java:${VERSION}
